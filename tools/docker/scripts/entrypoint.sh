8fcf39999558da7abeb40babe15f6db3a5f98ed8
nayanvanza91 <nayanvanza91@users.noreply.github.com>
2017-07-23 23:36:59 +0530

    Scripts to build an environment

---
+++ tools/docker/scripts/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+#create deployment user and set password
+if [ ! -f docker_initialized ]; then
+	useradd -s /bin/bash $USER
+	usermod -p $(echo $PASSWORD | openssl passwd -1 -stdin) $USER
+	touch docker_initialized
+fi
+
+exec "$@"
+#exec supervisord -n
